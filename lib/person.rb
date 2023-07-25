eccb91f4364457b8ca6f4326696f28291bfa31ff
kifle23 <kiflekifle23@gmail.com>
2023-07-25 13:58:21 +0300

    Update person class to nameable

--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,33 @@
+require_relative '../decorator/nameable'
+
 # Represents a person with a name, age, and parent permission status.
-class Person
-  attr_reader :id
-  attr_accessor :name, :age
-
-  def initialize(name = 'Unknown', age = 0, parent_permission: true)
-    @id = Random.rand(1..1000)
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-  end
-
-  def of_age?
-    @age >= 18
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  private :of_age?
+class Person < Nameable
+    attr_reader :id
+    attr_accessor :name, :age, :parent_permission
+  
+    def initialize(name = 'Unknown', age = 0, parent_permission: true)
+      super()
+      @id = Random.rand(1..1000)
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+    end
+  
+    def correct_name
+      @name
+    end
+  
+    def of_age?
+      @age >= 18
+    end
+  
+    def can_use_services?
+      of_age? || @parent_permission
+    end
+  
+    def to_s
+      "Person (id: #{@id}, name: #{@name}, age: #{@age}, parent_permission: #{@parent_permission})"
+    end
+  
+    private :of_age?
 end
