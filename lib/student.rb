ea2a6bba3a0233a845e1c21bedd9889e9896cc63
kifle23 <kiflekifle23@gmail.com>
2023-07-26 15:14:10 +0300

    Update Classroom and Student with a has-many/belongs-to relationship

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,19 @@
+require_relative 'person'
 # Represents a student with a name, age, parent permission status, and classroom.
 # Inherits from Person class.
+require_relative 'person'
+
 class Student < Person
   attr_reader :classroom
 
-  def initialize(name = 'Unknown', age = 0, parent_permission: true, classroom: 'Unknown')
-    super(name, age, parent_permission)
-    @classroom = classroom
+  def initialize(name = 'Unknown', age = 0, parent_permission: true, classroom: nil)
+    super(name, age, parent_permission: parent_permission)
+    @classroom = nil
+    self.classroom = classroom if classroom
+  end
+
+  def classroom=(new_classroom)
+    @classroom = new_classroom
   end
 
   def play_hooky
--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 require_relative 'lib/person'
+require_relative 'lib/class_room'
+require_relative 'lib/student'
 require_relative 'decorators/capitalize_decorator'
 require_relative 'decorators/trimmer_decorator'
+
 
 person = Person.new('maximilianus', 22)
 
@@ -9,3 +12,15 @@ capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
 puts capitalized_trimmed_person.correct_name
+math_class = Classroom.new('Math')
+student1 = Student.new('Alice', 16, parent_permission: true, classroom: math_class)
+english_class = Classroom.new('English')
+student2 = Student.new('Bob', 17, parent_permission: true, classroom: english_class)
+math_class.add_student(student1)
+english_class.add_student(student2)
+puts math_class.students.inspect
+puts english_class.students.inspect
+student3 = Student.new('Charlie', 15)
+math_class.add_student(student3)
+puts student3.classroom
+puts math_class.students
